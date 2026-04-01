0cc67274cf0cf7c6474f9d9b2cc61cc881b240a8
Abdiaziz-Mohawk <abdiazizmail1@gmail.com>
2026-04-01 10:25:50 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,13 @@
+#This is a multiplication function
 def multiply(a,b):
         result= a*b
         return result
 
 print(multiply(a,b))
+
+#This is an addition function
+def add(a,b)
+        result= a+b
+        return result
+
+print(add(a,b))
