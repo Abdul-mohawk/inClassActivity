c213ef2e059d8c04163d06a181adcce05e216a2b
Abdiaziz-Mohawk <abdiazizmail1@gmail.com>
2026-04-01 10:32:37 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,16 +1,20 @@
-#This is a multiplication function
+#This is a multiplication function Starting code before adding collaborator
 def multiply(a,b):
         result= a*b
         return result
 
-print(multiply(5,5))
 
-#This is an addition function
+
+#This is an addition function 000946400 Abdiaziz added this!
 def add(a,b):
         result= a+b
         return result
+        
 # This is the subtraction function 000964131 Alan added this!
 def subtract(a,b):
         result= a-b
         return result
+        
 print(add(5,5))
+print(multiply(5,5))
+print(subtract(5,5))
